5526fa85ad612c67b6683f75b1201ac746ff95ff
Andrew123-maker <142960669+Andrew123-maker@users.noreply.github.com>
2024-05-06 01:27:37 +0000

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,18 @@ class Post(models.Model):
   def __str__(self):
     return self.title
 
+class Sport(models.Model):
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  topic = models.CharField(max_length=20)
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  created_date = models.DateTimeField(default=timezone.now)
+  published_date = models.DateTimeField(blank=True, null=True)
+
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+  def __str__(self):
+    return self.title
+
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.post_list, name='post_list'),
+  path('', views.post_list, name='post_list'),
+  path('sports/', views.sports_discussion, name='sport_discussion'),
+  path('post/<int:pk>/', views.post_detail, name='post_detail'),
+  path('sports/post/<int:pk>/', views.sport_detail, name='post_detail'),
 ]
