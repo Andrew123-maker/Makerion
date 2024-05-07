80799acf0c5ec4f2eb536d25ef65f37339fba5ae
Andrew123-maker <142960669+Andrew123-maker@users.noreply.github.com>
2024-05-07 03:41:32 +0000

    fixing bugs on site

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.post_list, name='post_list'),
-  path('sports/', views.sports_discussion, name='sport_discussion'),
   path('post/<int:pk>/', views.post_detail, name='post_detail'),
-  path('sports/post/<int:pk>/', views.sport_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post, Sport
+from .models import Post
 
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
   return render(request, 'blog/post_list.html', {'posts':posts})
 
-def sports_discussion(request):
-  posts = Sport.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-  return render(request, 'blog/sport.html', {'posts':posts})
-
 def post_detail(request, pk):
   post = get_object_or_404(Post, pk=pk)
   return render(request, 'blog/post_detail.html', {'post':post})
 
-def sport_detail(request, pk):
-  post = get_object_or_404(Sport, pk=pk)
-  return render(request, 'blog/post_detail.html', {'post':post})
