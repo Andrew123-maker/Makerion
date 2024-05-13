cd268c9f488b08d72e7c406895218e6a2318b834
Andrew123-maker <142960669+Andrew123-maker@users.noreply.github.com>
2024-05-13 02:03:13 +0000

    new template and navbar

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,44 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.files import ImageField
 from django.utils import timezone
+from django.utils.text import slugify 
+from django.urls import reverse
+import uuid
+from django.db.models.signals import post_save, post_delete
+
+# uploading user files to a specific directory
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
+class Tag(models.Model):
+  title = models.CharField(max_length=100,verbose_name="tag")
+  slug = models.SlugField(null=False, unique=True, default=uuid.uuid1)
+  class Meta:
+    verbose_name = 'Tag'
+    verbose_name_plural = 'Tags'
+    
+  def get_absolute_url(self):
+    return reverse('tags', args=[self.slug])
+    
+  def __str__(self):
+    return self.title
+    
+  def save(self, *arg, **kwargs):
+    if not self.slug:
+      self.slug - slugify(self.slug)
+    return super().save(*arg, **kwargs)
+      
+  
 class Post(models.Model):
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  image = ImageField(default="default.jpg", blank=True)
   title = models.CharField(max_length=200)
-  topic = models.CharField(max_length=20, default='#Topic')
   text = models.TextField()
+  tag = models.ManyToManyField(Tag, related_name='tags')
+  likes = models.IntegerField(default = 0)
   created_date = models.DateTimeField(default=timezone.now)
   published_date = models.DateTimeField(blank=True, null=True)
 
@@ -17,3 +48,25 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+
+class Follow(models.Model):
+  follower = models.ForeignKey(User, on_delete=models.CASCADE,related_name='follower')
+  following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+class Stream(models.Model):
+  following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stream_following')
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post', null=True)
+  published_date = models.DateTimeField(blank=True, null=True)
+  
+  def add_post(sender, instance, *args, **kwargs):
+    post = instance
+    user = post.user
+    followers = Follow.objects.all().filter(following=user)
+    for follower in followers:
+      stream = Stream(post=post,user=follower.follower, published_date=post.published_date, following=user)
+      stream.save()
+
+
+#
+post_save.connect(Stream.add_post, sender=Post)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from .models import Post, Tag
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,4 +11,25 @@ def post_list(request):
 def post_detail(request, pk):
   post = get_object_or_404(Post, pk=pk)
   return render(request, 'blog/post_detail.html', {'post':post})
+
+def tag(request, tag_slug):
+  tag = get_object_or_404(Tag, slug=tag_slug)
+  posts = Post.objects.filter(tag=tag).order_by('-published_date')
+  context={
+    'tag': tag
+  }
+  return render(request, tag.html, context)
+
+def post_new(request):
+  if request.method == "POST":
+      form = PostForm(request.POST)
+      if form.is_valid():
+          post = form.save(commit=False)
+          post.author = request.user
+          post.published_date = timezone.now()
+          post.save()
+          return redirect('post_detail', pk=post.pk)
+  else:
+      form = PostForm()
+  return render(request, 'blog/post_edit.html', {'form': form})
 
